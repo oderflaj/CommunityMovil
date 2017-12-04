aebe2c9e22754d9ee0762d801241496fd068f109
Alfredo Carbajal <oderflaj@gmail.com>
2017-12-03 19:15:24 -0600

    Expo - Se logra adaptar Expo al desarrollo de esta manera se pueden compartir demos para que los usuario los puedan probar con su propio telefono. Icono - Se logra instalar los iconos en el emulador despues de que no se mostraban, probablemente era un error de cache del archivo

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-//Import a Library to help create a component
 import React, { Component } from 'react';
 import {  AppRegistry, 
           View, 
@@ -7,94 +6,22 @@ import {  AppRegistry,
           StyleSheet,
           AsyncStorage
         } from 'react-native';
-//import Login from './src/components/Login';
-//import Pages from './src/components/Pages';
 import BasePage from './src/components/BasePage';
 import * as RestOp from './src/components/functions/RestFunctions';
 
-
-
-
-// Generic Password, service argument optional
-//RestOp.login();
-
-//Create a component
-
-export default class App extends React.Component{
-  
-  // constructor(props)
-  // {
-  //   this.state={logedUser:false}
-  // }
-  
-
-  render(){
-    
-    // let x
-    
-    // await RestOp.isLoged().then(x =>JSON.parse(x)).then(obj=>{
-    //   console.log("----->>>>>",obj)
-    //   x = obj
-    // });
-    
-    ////console.log("Es una X->",x)
-    ////AsyncStorage.setItem('usuario','oderflaj@gmail.com')
-    //// AsyncStorage.getItem('usuario', (error, result) => {
-    ////   console.log(result);
-    ////   console.log(error);
-    //// });
-//
-    //// AsyncStorage.getItem('password', (error, result) => {
-    ////   console.log(result);
-    ////   console.log(error);
-    //// });
-
-    //// AsyncStorage.getItem('token', (error, result) => {
-    ////   console.log(result);
-    ////   console.log(error);
-    //// });
-
-    // if(x){
-    //   return(
-        
-    //           <View style={stylex.bgLogin}>
-    //             <Image 
-    //               style={stylex.bgLogin}
-    //               source={require('./src/image/bgLogin.png')}
-    //             />
-    //             <Login/>
-    //           </View>
-    //   );
-    // }
-    // else{
-    //   return(
-    //     <View>
-    //       <Text>
-    //         Pantalla nueva
-    //       </Text>
-    //     </View>
-    //   );
-    // }
-
-    return(
+export default class App extends React.Component {
+  render() {
+    return (
       <BasePage/>
     );
   }
 }
 
-const stylex =StyleSheet.create({
-  bgLogin:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-    position: 'absolute',
-    alignSelf: 'center',
-    height: '100%',
-    width: '100%'
-  }
+  },
 });
-
-
-//Render it to a device
-AppRegistry.registerComponent("Community", ()=>App);
-
--- src/components/modules/Alarm.js
+++ src/components/modules/Alarm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-// import MaterialIcons  from 'react-native-vector-icons/MaterialIcons';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+//import FontAwesome, { Icons } from 'react-native-fontawesome';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 class alarm extends Component {
   static navigationOptions = ({navigation})=>({
     title:'Menu',
@@ -20,13 +21,12 @@ class alarm extends Component {
           size={24}
           style={{color:'red'}}
         /> */}
+         {/* <FontAwesome>{Icons.chevronLeft}</FontAwesome> */}
         <Text>
           No es sano
+          <Icon name="ios-person" size={30} color="#4F8EF7" />
         </Text>
       </View>
-    //   <Icon.Button name="facebook" backgroundColor="#3b5998" onPress={this.loginWithFacebook}>
-    //   Login with Facebook
-    // </Icon.Button>
     );
   }
 }
